156e011ded03932930bcb387b0c2d48728b281e0
Gagan Jot Kour <kourg620@gmail.com>
2025-04-24 09:54:53 +0530

    Video Page

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,13 +1,35 @@
-import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { db } from "@/db";
 import { z } from "zod";
-import { videos, videoUpdateSchema } from "@/db/schema";
+import { users, videos, videoUpdateSchema } from "@/db/schema";
 import { mux } from "@/lib/mux";
-import { and, eq } from "drizzle-orm";
+import { and, eq, getTableColumns } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 import { UTApi } from "uploadthing/server";
 
 export const videosRouter = createTRPCRouter({
+
+    getOne: baseProcedure
+        .input(z.object({ id: z.string().uuid() }))
+        .query(async ({ input }) => {
+            const [ existingVideo ] = await db
+                .select({
+                    ...getTableColumns(videos),
+                    user: {
+                        ...getTableColumns(users),
+                    },
+                })
+                .from(videos)
+                .innerJoin(users, eq(videos.userId, users.id))
+                .where(eq(videos.id, input.id));
+                
+
+            if (!existingVideo) {
+                throw new TRPCError({ code: "NOT_FOUND" });
+            }    
+
+            return existingVideo;    
+        }),
 
     restoreThumbnail: protectedProcedure
         .input(z.object({ id: z.string().uuid() }))
